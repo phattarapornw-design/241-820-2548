087c2f0259239bfc4b15d42510ffa4501cf77b57
phattarapornw-design <phattaraporn.w@ku.th>
2025-12-29 17:09:40 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+/*
+loop statements
+while
+for
+*/
+
+/*
+let counter = 0;
+while(counter < 10){
+    console.log("hello world");
+    counter = counter + 1;
+}
+*/
+
+/*
+for(let i = 0; i < 10; i++){
+    console.log("hello world from for loop");
+}
+ let number1 = 20;
+ let number2 = 30;
+  let result = number1 + number2;
+    console.log("ผลบวก",result);
+*/
+/*
+let number = 20;
+ if(number % 2 == 0){
+    console.log("เป็นเลขคู่");
+}
+*/
+/*
+let number1 = 15;
+let number2 = 25;
+
+let condition1 = number1 == number2;
+console.log("result of condition",condition1);
+
+let number1 = 5;
+let number2 = 10;
+let condition = !(number1 >=3 || number2>=10);
+console.log("condition:",condition);
+
+let age = 20;
+let gender = "ชาย.";
+if(age >= 20 && gender == "ชาย."){
+    console.log("คุณเป็นผู้ชายที่มีอายุ 20 ปีขึ้นไป");
+{
+     */
