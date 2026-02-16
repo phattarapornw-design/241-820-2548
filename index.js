a09b495d59686c370f315b114397bb71e8965583
phattarapornw-design <phattaraporn.w@ku.th>
2026-02-16 16:50:14 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-//กำหนดport
-const http = require('http');
-const host = 'localhost';
-const port = 8000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const e = require('express');
+const { use } = require('react');
+const app = express();
+const port =  8000;
 
-//กำหนด server เมื่อเปิดใช้งาน
-const reqirelistener = function(req, res) {
-    res.writeHead(200);
-    res.end('my first server');
-}
-// run
-const server = http.createServer(reqirelistener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+app.use(bodyParser.json());
+
+let users = []
+let conter = 1;
+
+///path = get /users
+app.get('/users', (req, res) => {
+   res.json(users);
+});
+
+//path = Post /user
+    app.post('/user', (req, res) => {
+       let user = req.body;
+       user.id = conter
+       conter++;
+         users.push(user);
+      res.json({
+        message: 'User created successfully',
+        user: user});
+    });
+
+    //path = Put/user/:id
+    app.put('/user/:id', (req, res) => {
+        let id = req.params.id
+        //หา user ที่มี id ตรงกับ id ที่ส่งมา
+        let selectedUser = users.findIndex(user => {
+            if(user.id == id) {
+                return true
+            } else {
+                return false
+            }
+
+        } )
+        //อัพเดตข้อมูลของ user นั้น
+        users[selectedIndex] = updatedUser;
+
+        //ส่ง response กลับไปว่าอัพเดตสำเร็จ
+        
+        res.json({
+            message: 'User updated successfully',
+           data :{
+            user: updatedUser,
+            indexUpdated: selectedIndex
+           }
+        });
+
+        res.json(selectedIndex='');
+    });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
